8f8e4cad7f70704ae20a5b6fb94b95a66053c34a
Guru Swarupa <msgswarupa@gmail.com>
2026-06-04 13:35:41 +0530

    chore: update build configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.guruswarupa.launch"
         minSdk = 26
         targetSdk = 35
-        versionCode = 55
+        versionCode = 59
         versionName = "7.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
